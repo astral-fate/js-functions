2f6252e33d0ee7ef1ef3db9778d0f2a2f7131a1f
Fatimah Emad El-Den <63984422+xxfatj@users.noreply.github.com>
2022-01-01 16:02:38 +0300

    Add files via upload

---
+++ func.js
@@ -0,0 +1,25 @@
+
+var fruits = function(fruit){
+    return fruit * 4;
+
+ 
+    //return fruits;
+   
+          }
+
+ 
+        
+
+var items = function(apples, oranges){
+
+
+     let applescuts = fruits(apples);
+    let orangescuts = fruits(oranges);  
+    let results = console.log(`the output is ${applescuts} and ${orangescuts}`);            
+    
+    return results;
+    
+    }
+    console.log(items(2,3));
+
+ 
